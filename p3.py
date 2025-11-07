10da650ec96a673601d9b752aa09e8719d8c9e39
Mohit-devlop <mohit151106@gmail.com>
2025-11-07 12:30:58 +0530

    add

---
+++ p3.py
@@ -0,0 +1,23 @@
+data=bytearray([10,20,30,40,50])
+
+print("original array:")
+for value in data:
+    print(value,end="")
+for i in range(len(data)):
+    data[i]=data[i]+5
+
+print("\nmodified array:")
+
+for value in data:
+     print(value,end=" ")
+
+index=int(input("\n enter value of index(0-4):"))
+print("\nvalue at index",index,"is:",data[index])
+
+
+
+
+
+
+
+
