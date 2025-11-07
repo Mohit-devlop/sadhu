4ca830e89f5b4ab335c52c5f1a2fd49074181e68
Mohit-devlop <mohit151106@gmail.com>
2025-11-07 12:11:08 +0530

    add

--- p1.py
+++ p1.py
@@ -1,3 +1,9 @@
-a=10
-print(a)
-print("hello world")
+num1=int(input("enter number 1:"))
+num2=int(input("enter number 2:"))
+print("before swapping:")
+print("number 1:",num1)
+print("number 2:",num2)
+num1,num2=num2,num1
+print("after swapping:")
+print("number 1:",num1)
+print("number 2:",num2)
---
+++ p2.py
@@ -0,0 +1,11 @@
+num1r=float(input("enter number 1 real number :"))
+num1i=float(input("enter number 1 imaginary number :"))
+
+num2r=float(input("enter number 2real number :"))
+num2i=float(input("enter number 2 imaginary number :"))
+num1=complex(num1r,num1i)
+num2=complex(num2r,num2i)
+sum=num1+num2
+print("first complex number: ",num1)
+print("second complex number: ",num2)
+print("sum of  complex number is: ",sum)
